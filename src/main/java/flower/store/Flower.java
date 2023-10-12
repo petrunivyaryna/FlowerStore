513d487a588dafc764da8f8ed443a1d6b5a88e73
petrunivyaryna <petruniv.pn@ucu.edu.ua>
2023-10-12 21:51:36 +0300

    changes

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,19 +1,23 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-
-
-@Setter
+@NoArgsConstructor @AllArgsConstructor @Setter @Getter
 public class Flower {
-    @Getter
     private double sepalLength;
     private FlowerColor color;
-    @Getter
     private double price;
-    @Getter
     private FlowerType flowerType;
+
+    public Flower(Flower flower) {
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.color = flower.color;
+    }
 
     public String getColor() {
         return color.toString();
--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,25 @@
 package flower.store;
 
+import lombok.Getter;
+
+@Getter
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+    public FlowerPack(Flower flower, int quantity) {
+        this.flower = new Flower(flower);
+        this.quantity = quantity;
+    }
+    public void setQuantity(int quantityNew) {
+        if (quantityNew < 1) {
+            this.quantity = 1;
+        } else {
+            this.quantity = quantityNew;
+        }
+    }
+
+    public double getPrice() {
+        return flower.getPrice() * quantity;
+    }
 }
